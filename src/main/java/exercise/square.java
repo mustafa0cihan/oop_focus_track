0039cb5a5af8147a4ebec5f87e67eb6236d62f87
mustafa0cihan <mustafa0cihan@gmail.com>
2022-11-21 14:31:22 +0100

    Square exercise has been reviewed

--- src/main/java/exercise/square.java
+++ src/main/java/exercise/square.java
@@ -1,14 +1,20 @@
 package exercise;
 import java.util.Scanner;
 public class square {
+
+    public static int mySquareMethod ( int myNumber ){
+        return myNumber*myNumber;
+    }
+
     public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
 
             System.out.print("Please enter a number: ");
             int pNumber = scan.nextInt();
 
-            int result = pNumber*pNumber;
-            System.out.println("Square of your number : " + result);
+            int result = mySquareMethod(pNumber);
+
+        System.out.println("Square of your number : " + result);
         }
 }
 
